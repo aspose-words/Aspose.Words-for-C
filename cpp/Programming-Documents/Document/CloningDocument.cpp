21705437de83ecffc64f1fa344974bfb68681d69
Muzammil Khan <muzammil.khan@aspose.com>
2020-12-08 20:44:14 +0500

    Update CloningDocument.cpp

--- cpp/Programming-Documents/Document/CloningDocument.cpp
+++ cpp/Programming-Documents/Document/CloningDocument.cpp
@@ -2,6 +2,10 @@
 #include "examples.h"
 
 #include <Aspose.Words.Cpp/Model/Document/Document.h>
+#include <Aspose.Words.Cpp/Model/Document/DocumentBuilder.h>
+#include <Aspose.Words.Cpp/Model/Sections/Body.h>
+#include <Aspose.Words.Cpp/Model/Sections/Section.h>
+#include <Aspose.Words.Cpp/Model/Sections/SectionCollection.h>
 
 using namespace Aspose::Words;
 
@@ -9,20 +13,29 @@ void CloningDocument()
 {
     std::cout << "CloningDocument example started." << std::endl;
     // ExStart:CloningDocument
-    // The path to the documents directories.
-    System::String inputDataDir = GetInputDataDir_WorkingWithDocument();
-    System::String outputDataDir = GetOutputDataDir_WorkingWithDocument();
-
-    // Load the document from disk.
-    System::SharedPtr<Document> doc = System::MakeObject<Document>(inputDataDir + u"TestFile.doc");
-
+    // Create a Document.
+    System::SharedPtr<Document> doc = System::MakeObject<Document>();
+    System::SharedPtr<DocumentBuilder> builder = System::MakeObject<DocumentBuilder>(doc);
+    builder->Writeln(u"This is the original document before applying the clone method");
+    
+    // Clone the document.
     System::SharedPtr<Document> clone = doc->Clone();
 
-    System::String outputPath = outputDataDir + u"CloningDocument.doc";
+    // Edit the cloned document.
+    builder = System::MakeObject<DocumentBuilder>(clone);
+    builder->Write(u"Section 1");
+    builder->InsertBreak(BreakType::SectionBreakNewPage);
+    builder->Write(u"Section 2");
 
-    // Save the document to disk.
-    clone->Save(outputPath);
+    // This shows what is in the document originally. The document has two sections.
+    std::cout << clone->GetText().Trim() << std::endl << std::endl;
+
+    // Duplicate the last section and append the copy to the end of the document.
+    auto lastSectionIdx = clone->get_Sections()->get_Count() - 1;
+    System::SharedPtr<Section> newSection = clone->get_Sections()->idx_get(lastSectionIdx)->Clone();
+    clone->get_Sections()->Add(newSection);
+
+    // Check what the document contains after we changed it.
+    std::cout << clone->GetText().Trim() << std::endl << std::endl;
     // ExEnd:CloningDocument
-    std::cout << "Document cloned successfully." << std::endl << "File saved at " << outputPath.ToUtf8String() << std::endl;
-    std::cout << "CloningDocument example finished." << std::endl << std::endl;
 }
