17c95b95a3954971da20f68980b73ef36b6e70be
Evgeny Vasilev <evgeny.vasilev@aspose.com>
2023-09-23 00:10:05 +0400

    Update assertion check

--- Examples/ApiExamples/source/ExPdfSaveOptions.h
+++ Examples/ApiExamples/source/ExPdfSaveOptions.h
@@ -700,7 +700,7 @@ public:
         doc->Save(ArtifactsDir + u"PdfSaveOptions.HandleBinaryRasterWarnings.pdf", saveOptions);
 
         ASSERT_EQ(1, callback->Warnings->get_Count());
-        ASSERT_EQ(u"'R2_XORPEN' binary raster operation is partly supported.", callback->Warnings->idx_get(0)->get_Description());
+        ASSERT_EQ(u"'R2_XORPEN' binary raster operation is not supported.", callback->Warnings->idx_get(0)->get_Description());
     }
 
     /// <summary>
